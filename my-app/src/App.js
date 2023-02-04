6e921cea6aeb960efe063779d785bafb4bc42913
Mustan Ali <mustan9900@gmail.com>
2023-02-04 12:20:02 +0500

    add favicon

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -23,11 +23,13 @@ function App() {
     if (mode === 'light') {
       setMode('dark');
       document.body.style.backgroundColor = '#333333';
+      document.title = "TextUtils - Dark Mode";
       showAlert(" Dark Mode enabled ", "success");
     }
     else {
       setMode('light');
       document.body.style.backgroundColor = 'white';
+      document.title = "TextUtils - Light Mode";
       showAlert(" Light Mode enabled ", "success");
     }
   }
